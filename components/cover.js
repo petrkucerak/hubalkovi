bea67392c7f0043e17785a0815e299bff03552cb
petrkucerak <petr@khome.cz>
2023-07-18 11:41:48 +0200

    Improve responsibility

--- components/cover.js
+++ components/cover.js
@@ -1,19 +1,17 @@
 export default function Cover() {
   return (
-    <div
-      className={`bg-[#000] bg-cover bg-center w-full h-[100vh] bg-[url('/images/cover.webp')] flex justify-around items-center flex-col`}
-    >
+    <div className="bg-[#000] bg-cover bg-center w-full h-[100vh] bg-[url('/images/cover.webp')] flex justify-around items-center flex-col">
       <div className={`bg-[#000] opacity-40 absolute w-full h-[100vh]`}></div>
-      <h1 className="text-white text-3xl uppercase z-10 font-quicksand font-light tracking-widest">
+      <h1 className="text-white text-2xl sm:text-3xl uppercase z-10 font-quicksand font-light tracking-widest text-center max-w-[600px] w-[80vw]">
         Budeme se brát
       </h1>
       <img
         src="/images/title-white.svg"
-        className="max-w-[600px] w-[90vw] z-10"
+        className="max-w-[600px] w-[80vw] z-10"
       />
-      <h1 className="text-white text-5xl uppercase z-10 font-quicksand font-light">
+      <p className="text-white text-2xl sm:text-5xl uppercase z-10 font-quicksand font-light text-center max-w-[600px] w-[80vw]">
         30. 9. 2023
-      </h1>
+      </p>
     </div>
   );
 }
