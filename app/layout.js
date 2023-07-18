b63f44c5d7a51de12de96c33df6e06fd26ea80b2
petrkucerak <petr@khome.cz>
2023-07-18 11:32:03 +0200

    Change title

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Hubálkovi",
+  description: "Budeme se brát!",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="cs">
--- app/page.js
+++ app/page.js
@@ -1,29 +1,7 @@
-import Head from "next/head";
 import Cover from "../components/cover";
 export default function Home() {
   return (
     <main className="">
-      <Head>
-        <title>Hubálkovi</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
       <Cover />
     </main>
   );
